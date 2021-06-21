08202308bc4c6b7e4314d884ef54091fde8ce04b
Gaya1858 <gayathri1858@yshoo.com>
2021-06-21 10:17:36 -0400

    day2-Project done

--- daytwo/day2_project.py
+++ daytwo/day2_project.py
@@ -24,9 +24,24 @@ print(f"your score is {score}, your height is {height}, you are winning is {isWi
 Youe Life in Weeks program
 if we live until 90 years old
 '''
+'''
 age = int(input("eneter your age: "))
 age_remain =90-age
 days = age_remain*365
 weeks = age_remain*52
 months =age_remain*12
-print(f"You have {days} days, {weeks} weeks, and {months} months left")
+print(f"You have {days} days, {weeks} weeks, and {months} months left")'''
+'''
+Day2 final project - calaculating tips 
+
+'''
+bill_amount = float(input("Enter your total bill\n"))
+tip_percent = float(input("Enter the percentage of the tip you wnat to pay\n"))
+num_people = int(input("Enter the number of people to split the bill amount\n"))
+
+tip = (bill_amount*tip_percent)/100
+ttl = bill_amount+tip
+split = ttl/num_people
+print(f"The bill amount is: {bill_amount},\nthe tip percentage is: {tip_percent} "
+      f"= {tip},\ntotal amount to be paid including tips: {ttl},\n"
+      f"total number people to split: {num_people},\nand for each have to pay: {split}.")
