5b864b65d208f46d0f3ba39064d9763a4a616dfa
Gaya1858 <gayathri1858@yshoo.com>
2021-06-23 16:42:39 -0400

    day6 lessons over

---
+++ daysix/day6_lesson.py
@@ -0,0 +1,18 @@
+'''
+while loop
+'''
+def my_function():
+    print("Hello")
+    print("bye")
+
+
+my_function()
+
+# while something is true do this
+x =6
+while x>0:
+    print("I love Karthik!")
+    x -=1
+# for loops are great when you are iterating through some lists
+# while loops are little bit more dangerous, because it will run until something not true or true
+# according to the program. It may end up infinite run if you dont have proper stop case.
