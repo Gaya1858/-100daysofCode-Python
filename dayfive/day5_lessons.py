d122905fbd7d58e94a4b15e9c06801c5fe1b65cf
Gaya1858 <gayathri1858@yshoo.com>
2021-06-23 12:37:35 -0400

    day5_lessons.py

---
+++ dayfive/day5_lessons.py
@@ -0,0 +1,11 @@
+'''
+for loop and while loop
+
+'''
+
+names =["gaya", "saya","liya","raya","jaya","piya","kaya"]
+
+for name in names:
+    print(name)
+for i in range(0, len(names),3):
+    print(names[i])
