22ef36f9bdfd2676d929724f93320bf68dff1888
Gaya1858 <gayathri1858@yshoo.com>
2021-06-30 19:56:39 -0400

    Blackjack game halfway through

--- dayeleven/BlackJack/__init__.py
+++ dayeleven/BlackJack/__init__.py
@@ -8,26 +8,64 @@ This class will provide supporting function for the blackjack game
 import random
 
 jack_list = [11,2,3,4,5,6,7,8,9,10,10,10,10]
+card_player =[]
+card_comp =[]
 
 def card_player():
+    card_player1 =[]
     card_one = random.randint(0,len(jack_list))
     card_two = random.randint(0,len(jack_list))
-    card1 =jack_list[card_one]
-    card2 =jack_list[card_two]
-    return card1,card2
+    card_player1.append(jack_list[card_one])
+    card_player1.append(jack_list[card_two])
+    card_player =card_player1
+
+    return card_player
 
 def card_system():
+    card_comp1 =[]
     card_one = random.randint(0,len(jack_list))
     card_two = random.randint(0,len(jack_list))
-    card1 =jack_list[card_one]
-    card2 =jack_list[card_two]
-    return card1,card2
+    card_comp1.append(jack_list[card_one])
+    card_comp1.append(jack_list[card_two])
+    card_comp =card_comp1
+    return card_comp
 
 def player_hit():
+    card_player1 =[]
     card_one = random.randint(0, len(jack_list))
-    card1 = jack_list[card_one]
-    return card1
+    card_player1.append(jack_list[card_one])
+    card_player2 = card_player+card_player1
+    return card_player2
+
 def system_hit():
+    card_comp1 =[]
     card_one = random.randint(0, len(jack_list))
-    card1 = jack_list[card_one]
-    return card1
+    card_comp1.append(jack_list[card_one])
+    card_comp2 =card_comp+card_comp1
+    return card_comp2
+
+def check_jack():
+    i =sum_list(card_player)
+    if(i < 21):
+        return 0
+    elif i==21:
+        return 1
+    else:
+        return 3
+def sum_list(lists):
+    sum = 0
+    for i in lists:
+        sum += i
+    return sum
+def stand_jack():
+    if len(card_comp) ==2:
+        sum = sum_list(card_comp)
+        if(sum <21):
+            system_hit()
+            sum = sum_list(card_comp)
+            if(sum <=21 and sum_list(card_player)> sum):
+                return 1
+            else:
+                return 0
+
+
