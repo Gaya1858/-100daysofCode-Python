a7cb532b3a1628e6f57677da192ff125a3f35315
Gaya1858 <gayathri1858@yshoo.com>
2021-07-11 07:56:57 -0400

    machine status updated

--- day21_tuple/hirst-painting/spot_paint.py
+++ day21_tuple/hirst-painting/spot_paint.py
@@ -11,11 +11,13 @@ def draw_dots(turt,row,col,colors):
     for i in range(0,row):
 
         for j in range(0,col):
+
             x =random_color(colors)
             turt.fillcolor(x)
             turt.begin_fill()
             turt.pencolor(x)
             turt.pendown()
+            turt.speed(10)
             turt.circle(5)
             turt.end_fill( )
             turt.penup()
@@ -30,7 +32,7 @@ def random_color(colors):
     ran = random.randint(0,len(colors)-1)
     return colors[ran].rgb
 # Extract 6 colors from an image.
-colors =colorgram.extract("image.jpg",5)
+colors =colorgram.extract("image.jpg",15)
 
 # colorgram.extract returns Color objects, which let you access
 # RGB, HSL, and what proportion of the image was that color.
@@ -39,8 +41,9 @@ rgb = first_color.rgb # e.g. (255, 151, 210)
 hsl = first_color.hsl # e.g. (230, 255, 203)
 proportion  = first_color.proportion # e.g. 0.34
 turt = t.Turtle()
+
 t.colormode(255)
 my_screen =t.Screen()
-draw_dots(turt,5,5,colors)
+draw_dots(turt,15,15,colors)
 my_screen.exitonclick()
 
